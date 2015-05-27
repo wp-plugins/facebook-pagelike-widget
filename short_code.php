cdc7f172e4c0c78a3cf2468904e49d4937fca415
Milap <Milap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-27 16:15:29 +0000

    short_code.php updated for display changes.
    git-svn-id: https://plugins.svn.wordpress.org/facebook-pagelike-widget/trunk@1168693 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- short_code.php
+++ short_code.php
@@ -4,7 +4,7 @@ function fb_plugin_shortcode() {
     $widget_facebook_widget = get_option('widget_fbw_id');
     if (is_active_widget(false, '', 'fbw_id')) {
         foreach ($widget_facebook_widget as $facebook_widget) {
-
+            
             $title = $facebook_widget['title'];
             $app_id = $facebook_widget['app_id'];
             $fb_url = $facebook_widget['fb_url'];
@@ -17,23 +17,24 @@ function fb_plugin_shortcode() {
             $show_border            =	$facebook_widget['show_border'];
             $custom_css             =	$facebook_widget['custom_css'];
             $select_lng             =	$facebook_widget['select_lng'];
-            echo $before_widget;
+            $result = $before_widget;
             if ($title)
-                echo $before_title . $title . $after_title;
+                $result .= $before_title . $title . $after_title;
 
             wp_register_script('myownscript', FB_WIDGET_PLUGIN_URL . 'fb.js', array('jquery'));
             wp_enqueue_script('myownscript');
             $local_variables = array('app_id' => $app_id,'select_lng'=>$select_lng);
             wp_localize_script('myownscript', 'vars', $local_variables);
-            echo '<div id="fb-root"></div>
+            $result .= '<div id="fb-root"></div>
             <div class="fb-like-box" data-href="'.$fb_url.'" data-width="'.$width.'" data-height="'.$height.'" data-colorscheme="'.$color_scheme.'" data-show-faces="'.$show_faces.'" data-header="'.$show_header.'" data-stream="'.$show_stream.'" data-show-border="'.$show_border.'" style="'.$custom_css.'"></div>';
-            echo $after_widget;
+            $result .= $after_widget;
+            return $result;
         }
     }
     else {
-        echo "Please configure Widget first..!!";
+        $result .= "Please configure Widget first..!!";
+        return $result;
     }
-}
-
+ }
 add_shortcode('fb_widget','fb_plugin_shortcode');
 ?>
